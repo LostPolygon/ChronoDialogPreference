7b93f19e000977a4236281b2790ef9c63a938ba9
Serhii Yolkin <serhii.yolkin@gmail.com>
2023-11-24 19:38:50 +0100

    refactor: move fragment handling to separate class

--- chronodialogpreference/src/main/java/com/app/dr1009/chronodialogpreference/ChronoPreferenceFragment.java
+++ chronodialogpreference/src/main/java/com/app/dr1009/chronodialogpreference/ChronoPreferenceFragment.java
@@ -1,11 +1,10 @@
 package com.app.dr1009.chronodialogpreference;
 
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.timepicker.MaterialTimePicker;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -14,41 +13,25 @@ public abstract class ChronoPreferenceFragment extends PreferenceFragmentCompat 
     public static final String DIALOG_FRAGMENT_TAG = "ChronoPreferenceFragment.DIALOG";
     static final String LOG_TAG = "ChronoPreference";
     static final String ARG_PREFERENCE_KEY = "key";
+    private ChronoPreferenceFragmentHandler mFragmentHandler;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFragmentHandler = new ChronoPreferenceFragmentHandler(this);
+    }
 
     @Override
     public void onResume() {
         super.onResume();
 
-        final Fragment fragmentByTag = getParentFragmentManager().findFragmentByTag(DIALOG_FRAGMENT_TAG);
-        if (fragmentByTag instanceof MaterialDatePicker) {
-            //noinspection unchecked
-            MaterialDatePickerWrapper.attachDateDialogFragmentListeners(
-                (MaterialDatePicker<Long>) fragmentByTag,
-                getPreferenceFromFragment(fragmentByTag)
-            );
-        } else if (fragmentByTag instanceof MaterialTimePicker) {
-            MaterialTimePickerWrapper.attachTimeDialogFragmentListeners(
-                (MaterialTimePicker) fragmentByTag,
-                getPreferenceFromFragment(fragmentByTag)
-            );
-        }
-    }
-
-    private <T extends Preference> T getPreferenceFromFragment(Fragment fragment) {
-        assert fragment.getArguments() != null;
-        String preferenceKey = fragment.getArguments().getString(ARG_PREFERENCE_KEY);
-        assert preferenceKey != null;
-        return findPreference(preferenceKey);
+        mFragmentHandler.onResume();
     }
 
     @Override
     public void onDisplayPreferenceDialog(@NonNull Preference preference) {
-        DialogFragment dialogFragment = null;
-        if (preference instanceof TimeDialogPreference) {
-            dialogFragment = MaterialTimePickerWrapper.createTimeDialogFragment((TimeDialogPreference) preference);
-        } else if (preference instanceof DateDialogPreference) {
-            dialogFragment = MaterialDatePickerWrapper.createDateDialogFragment((DateDialogPreference) preference);
-        }
+        DialogFragment dialogFragment =
+            ChronoPreferenceFragmentHandler.createDisplayPreferenceDialog(preference);
 
         if (dialogFragment != null) {
             //noinspection deprecation
